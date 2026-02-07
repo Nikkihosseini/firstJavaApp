4f9abc77c0ee0488b03ee683c2c814af28e1c6c1
Nikkihosseini <niekal00@gmail.com>
2026-02-07 15:10:20 +0330

    feat: difference between print and println

---
+++ src/chapter2/Exercise2.java
@@ -0,0 +1,11 @@
+package chapter2;
+
+public class Exercise2{
+    public static void main(String[] args){
+        System.out.print("Java is a good language ");
+        System.out.println("and It Is my favorite");
+        System.out.println("This is my first program in Java");
+        System.out.println();
+        System.out.println("I wrote in InteliJ");
+    }
+}
