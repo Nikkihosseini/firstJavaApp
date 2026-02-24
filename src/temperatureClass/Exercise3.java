c84f2cb056a36d6bde6f6c7521681145d84f527c
Nikkihosseini <niekal00@gmail.com>
2026-02-24 16:28:17 +0330

    feat: implement Temperature class with getter and setter methods

---
+++ src/temperatureClass/Exercise3.java
@@ -0,0 +1,13 @@
+package temperatureClass;
+
+
+public class Exercise3 {
+    public static void main(String[] args){
+
+        var temp = new Temperature();
+
+        temp.setTemperature(25.5);
+        System.out.println("Temperature is: " + temp.getTemperature());
+
+    }
+}
