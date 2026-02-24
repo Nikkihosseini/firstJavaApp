e13b386263156a9c05d431754ab7ac0980ae03d1
Nikkihosseini <niekal00@gmail.com>
2026-02-24 19:01:04 +0330

    refactor: initialize Wage fields via constructor instead of default values

--- src/wageClass/Exercise4.java
+++ src/wageClass/Exercise4.java
@@ -7,12 +7,12 @@ public class Exercise4 {
     public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
+        var wage = new Wage(50_000, 5_000);
 
         int extraHours;
         System.out.print("enter extra hours you worked: ");
         extraHours = scanner.nextInt();
 
-        var wage = new Wage();
         wage.setExtraHours(extraHours);
         System.out.println("Total wage is: " + wage.getExtraHours());
     }
